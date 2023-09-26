7af794633d01e14b4dede1ea1088e1bfa67b8677
Fiz Rey Armesto <39385273+ficho12@users.noreply.github.com>
2023-09-26 21:47:46 +0200

    Added 1.BinaryGap.cs

--- Exercises/Bit-Ops/1.BinaryGap.cs
+++ Exercises/Bit-Ops/1.BinaryGap.cs
@@ -5,11 +5,30 @@ using System;
 // you can write to stdout for debugging purposes, e.g.
 // Console.WriteLine("this is a debug message");
 
+// Results: 100% Correctness, Performance: Not assessed
+
 // See Lesson 1 - Iterations - BinaryGap (Repeated exercise) 
 
 class Solution {
     public int solution(int N) {
         //Convert N to binary
         string binary = Convert.ToString(N, 2);
+        int len = binary.Length;
+        int maxGap = 0, currentGap = 0;
+
+        for (int i = 0; i < len; i++)
+        {
+            if (binary[i] == '1')
+            {
+                if(currentGap>=maxGap)
+                    maxGap = currentGap;
+                currentGap = 0;
+            }
+            else
+            {
+                currentGap++;
+            }
+        }
+        return maxGap;
     }
 }
